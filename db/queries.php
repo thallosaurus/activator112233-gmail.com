0a13a1633788f49defd4f35cc84bcab1e6edf955
Philipp Gaßner <activator112233@gmail.com>
2020-03-25 01:24:50 +0100

    Set sorting to descending
    Now I can sleep in peace

--- db/queries.php
+++ db/queries.php
@@ -1,7 +1,7 @@
 <?php
 $GLOBALS["lm"]["QRY"]["find_in_radius"]
 //= "SELECT id, name, db_lat, db_lon, ROUND(6371 * ACOS( COS( RADIANS( db_lat ) ) * COS( RADIANS( :u_lat ) ) * COS( RADIANS( :u_lon ) - RADIANS( db_lon ) ) + SIN( RADIANS( db_lat ) ) * SIN( RADIANS( :u_lat ) ) ), 4) AS distance FROM coordinates HAVING distance <= :u_rad ORDER BY distance ASC";
-= "SELECT coordinates.id, coordinates.ts AS timestamp, coordinates.db_lat AS db_lat, coordinates.db_lon AS db_lon, posts.title, posts.category, posts.type, ROUND(6371 * ACOS( COS( RADIANS( db_lat ) ) * COS( RADIANS( :u_lat ) ) * COS( RADIANS( :u_lon ) - RADIANS( db_lon ) ) + SIN( RADIANS( db_lat ) ) * SIN( RADIANS( :u_lat ) ) ), 4) AS distance FROM coordinates JOIN posts ON coordinates.id = posts.id HAVING distance <= :u_rad ORDER BY timestamp ASC";
+= "SELECT coordinates.id, coordinates.ts AS timestamp, coordinates.db_lat AS db_lat, coordinates.db_lon AS db_lon, posts.title, posts.category, posts.type, ROUND(6371 * ACOS( COS( RADIANS( db_lat ) ) * COS( RADIANS( :u_lat ) ) * COS( RADIANS( :u_lon ) - RADIANS( db_lon ) ) + SIN( RADIANS( db_lat ) ) * SIN( RADIANS( :u_lat ) ) ), 4) AS distance FROM coordinates JOIN posts ON coordinates.id = posts.id HAVING distance <= :u_rad ORDER BY timestamp DESC";
 
 $GLOBALS["lm"]["QRY"]["post_data"]
     = "INSERT INTO `posts`(`title`, `content`, `type`, `category`) VALUES (:title, :content, :type, :category)";
